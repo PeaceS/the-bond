34c1c10e7d373c0959655aac4999507a1464ef49
PeaceS <p.saravudecha@gmail.com>
2025-09-13 17:37:44 +0700

    fix: js

--- public/main.js
+++ public/main.js
@@ -1,59 +1,58 @@
-function formatId(id) {
-  const idAsString = String(id);
-  const paddedId = idAsString.padStart(3, '0');
-
-  return `TB-${paddedId}`;
-}
-
-async function fetchProducts() {
-  try {
-    if (isLoading) return; // Prevent multiple simultaneous requests
-    isLoading = true;
-
-    const response = await fetch('/products');
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    const products = await response.json();
-    const productContainer = document.getElementById('product-container');
-    const productTemplate = document.getElementById('product-template');
-
-    products.forEach(product => {
-      const productDiv = productTemplate.cloneNode(true);
-      productDiv.removeAttribute('id');
-
-      titleDiv = productDiv.querySelector('.product-title');
-      titleDiv.textContent = formatId(product.id);
-
-      imageDiv = productDiv.querySelector('.product-image');
-      imageDiv.src = `images/the-bond/${product.id}.png`;
-      imageDiv.alt = formatId(product.id);
-
-      descriptionDiv = productDiv.querySelector('.product-description');
-      descriptionDiv.textContent = product.description;
-
-      priceDiv = productDiv.querySelector('.product-price');
-      priceDiv.classList.add(product.currency);
-      priceDiv.textContent = product.price;
-
-      productContainer.appendChild(productDiv);
-    });
-
-    productContainer.removeChild(productTemplate);
-  } catch (error) {
-    console.error('Failed to fetch products:', error);
-  } finally {
-    isLoading = false;
-  }
-}
-
 document.addEventListener('DOMContentLoaded', () => {
-  const productsContainer = document.getElementById('products-container');
+  const productContainer = document.getElementById('products-container');
   let isLoading = false;
 
   const apiURL = '/products';
+
+  function formatId(id) {
+    const idAsString = String(id);
+    const paddedId = idAsString.padStart(3, '0');
+  
+    return `TB-${paddedId}`;
+  }
+  
+  async function fetchProducts() {
+    try {
+      if (isLoading) return; // Prevent multiple simultaneous requests
+      isLoading = true;
+  
+      const response = await fetch(apiURL);
+  
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+  
+      const products = await response.json();
+      const productTemplate = document.getElementById('product-template');
+  
+      products.forEach(product => {
+        const productDiv = productTemplate.cloneNode(true);
+        productDiv.removeAttribute('id');
+  
+        titleDiv = productDiv.querySelector('.product-title');
+        titleDiv.textContent = formatId(product.id);
+  
+        imageDiv = productDiv.querySelector('.product-image');
+        imageDiv.src = `images/the-bond/${product.id}.png`;
+        imageDiv.alt = formatId(product.id);
+  
+        descriptionDiv = productDiv.querySelector('.product-description');
+        descriptionDiv.textContent = product.description;
+  
+        priceDiv = productDiv.querySelector('.product-price');
+        priceDiv.classList.add(product.currency);
+        priceDiv.textContent = product.price;
+  
+        productContainer.appendChild(productDiv);
+      });
+  
+      productContainer.removeChild(productTemplate);
+    } catch (error) {
+      console.error('Failed to fetch products:', error);
+    } finally {
+      isLoading = false;
+    }
+  }
 
   fetchProducts();
 
