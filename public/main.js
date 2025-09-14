aff1343e7f33c8970fa590636bd6695a83f5fc44
Pentor Saravudecha <p.saravudecha@gmail.com>
2025-09-14 16:35:01 +0700

    Sliceshow (#2)
    * feat: add sliceshow
    
    * fix: css a bit
    
    * fix: css
    
    * fix: css + debug a bit
    
    * fix: js
    
    * fix: try icon
    
    * fix: add icon
    
    * fix: css
    
    * fix: css
    
    * fix: css

--- public/main.js
+++ public/main.js
@@ -39,18 +39,52 @@ document.addEventListener('DOMContentLoaded', () => {
         titleDiv.textContent = formatId(product.id);
   
         imageDiv = productDiv.querySelector('.product-image');
-        imageDiv.src = `images/the-bond/${product.id}.png`;
+        imageDiv.src = `images/the-bond/${product.id}/1.png`;
         imageDiv.alt = formatId(product.id);
-  
+        imageDiv.id = formatId(product.id)
+
         descriptionDiv = productDiv.querySelector('.product-description');
         descriptionDiv.textContent = product.description;
   
         priceDiv = productDiv.querySelector('.product-price');
         priceDiv.classList.add(product.currency);
         priceDiv.textContent = product.price;
+
+        let currentImageIndex = 0;
+        const images = [
+          `images/the-bond/${product.id}/1.png`,
+          `images/the-bond/${product.id}/2.png`,
+          `images/the-bond/${product.id}/3.png`
+        ];
+
+        const updateImage = (id) => {
+          imageDiv = document.getElementById(id);
+          imageDiv.classList.remove('show');
+          console.log(imageDiv);
+  
+          // Wait for the fade-out to complete before changing the image source
+          setTimeout(() => {
+              imageDiv.src = images[currentImageIndex];
+              // Fade in the new image
+              imageDiv.classList.add('show');
+          }, 500); // This delay should match the CSS transition duration
+        };
+
+        prevBtn = productDiv.querySelector('.previous-btn');
+        prevBtn.addEventListener('click', () => {
+          currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+          updateImage(formatId(product.id));
+        });
+
+        nextBtn = productDiv.querySelector('.next-btn');
+        nextBtn.addEventListener('click', () => {
+          currentImageIndex = (currentImageIndex + 1) % images.length;
+          updateImage(formatId(product.id));
+        });
   
         productContainer.appendChild(productDiv);
         loadedBondIds.push(product.id);
+
       });
   
       productTemplate.classList.add('hide');
