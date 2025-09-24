662abb6af8384b08fb20d1bcbae238d56ed07b41
PeaceS <p.saravudecha@gmail.com>
2025-09-24 14:33:44 +0700

    fix: more logs

--- functions/products.js
+++ functions/products.js
@@ -11,9 +11,11 @@ export async function onRequestGet(context) {
     if (excludeIdsString) {
       // Split the string into an array of IDs and convert them to numbers
       const excludeIds = excludeIdsString.split(',').map(id => parseInt(id, 10));
+      console.log(excludeIds);
       
       // Create a string of placeholders (?, ?, ?) for the IN clause
       const placeholders = excludeIds.map(() => '?').join(', ');
+      console.log(placeholders);
 
       // Append the NOT IN clause to the query
       query += ` AND id NOT IN (${placeholders})`;
