a04c1bd0911ca29f08f5bb1267e87780570e9b5f
PeaceS <p.saravudecha@gmail.com>
2025-09-30 15:20:06 +0700

    feat: add stripe

---
+++ functions/session.js
@@ -0,0 +1,31 @@
+import Stripe from 'stripe';
+
+export async function onRequestPost(context) {
+    const { env, request } = context;
+    const stripe = new Stripe(env.STRIPE_SECRET_KEY);
+    const priceId = env.PRICE_ID;
+
+    try {
+        const protocol = 'https';
+        const host = request.headers.get('host');
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ['card'],
+            line_items: [{
+                price: priceId,
+                quantity: 1,
+            }],
+            mode: 'payment',
+            success_url: `${protocol}://${host}/success`,
+            cancel_url: `${protocol}://${host}/cancel`
+        });
+
+        return new Response(JSON.stringify({ url: session.url }), {
+            status: 200,
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+    } catch (error) {
+        console.log(error.message);
+        return new Response(JSON.stringify({ error: error.message }), { status: 500 });
+    }
+}
