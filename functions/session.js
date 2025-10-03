629dbeb34a18eeabf5e9e1e588815147c832d58a
PeaceS <p.saravudecha@gmail.com>
2025-10-03 15:41:26 +0700

    fix: session link

--- functions/session.js
+++ functions/session.js
@@ -15,7 +15,7 @@ export async function onRequestPost(context) {
                 quantity: 1,
             }],
             mode: 'payment',
-            success_url: `${protocol}://${host}/success`,
+            success_url: 'https://artmuc.rungploy.com/success?session_id={CHECKOUT_SESSION_ID}',
             cancel_url: `${protocol}://${host}/cancel`,
             metadata: {
               product_type: 'bonds',
